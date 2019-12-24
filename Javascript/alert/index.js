bc0947c754e2fef4a981cd65a8f9b1b9211b1635
zarakM <mughalzarak@gmail.com>
2019-12-24 20:47:46 +0500

    alert done

---
+++ Javascript/alert/index.js
@@ -0,0 +1,14 @@
+// // alert("welcome")
+
+// var user = "zarak"
+
+// alert(user)
+
+// user = "zain"
+
+// user
+// // console.log(user)
+
+var quote = "Your's sincerly"
+//your's sincerly
+console.log(quote)
