d0afa5af38e4cf65ee6d95b2b53cda8866190d1c
zarakM <mughalzarak@gmail.com>
2019-12-24 21:27:54 +0500

    variables done

---
+++ Javascript/variables/index.js
@@ -0,0 +1,19 @@
+// var operand_one = 80
+// var operand_two = 50
+
+// console.log(operand_one + operand_two)
+
+// var original_price = 1000
+// var discounted_price = original_price- 200
+
+// console.log(discounted_price)
+
+var number_one = 1
+var number_two = 1
+var number_three = "zarak"
+
+console.log(number_three + number_one - number_two)
+
+var user = "zarak"
+
+console.log("welcome : " + user)
