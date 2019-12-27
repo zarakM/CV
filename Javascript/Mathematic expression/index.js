27bb9b6ef0014ea575979b2850cc7f8e420af3a2
zarakM <mughalzarak@gmail.com>
2019-12-27 21:42:19 +0500

    pre increment and post increment decremnt

---
+++ Javascript/Mathematic expression/index.js
@@ -0,0 +1,37 @@
+// var plus = 2 + 2
+// var minus = 2 - 2
+// var divide = 2 / 2
+// var multiply = 2 * 2
+
+// var answer = 5 + plus
+
+// console.log(answer)
+
+// var input = 0
+
+// var odd = input % 1
+
+// console.log(odd)
+
+// var num = 5
+
+// console.log(++num)
+
+// //pre increment
+// var num = 5
+// num = num + 1
+// console.log( num )
+
+// //post increment
+// var num = 5
+// console.log( num )
+// num = num + 1
+
+
+var value = 3
+var sum =++value
+
+// console.log(++value) // show=3 value= 4
+
+console.log(sum)   //4
+console.log(value) //4
---
+++ Javascript/legal or illegal/index.js
@@ -0,0 +1,20 @@
+// var welcom e = "hello i am zarak" //illegal
+// var alert = "zarak"  //illegal
+
+// var needToWelcome     //camel case
+// var need_to_welcome   // underscore case
+// var needtowelcome     
+
+
+// var $welcome = "hello i am zarak"
+// var _welcome = "hello i am zarak"
+
+// var name = "zarak"
+// var Name = "zain"
+
+// console.log(name)
+// console.log(Name)
+
+var user ="zarak"
+
+alert("Greetings to: "+ x)
