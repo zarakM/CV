2b718101a37923844e233c80496a41a854d3d8bf
zarakM <mughalzarak@gmail.com>
2020-02-11 21:24:13 +0500

    Date Object Javascript

---
+++ Javascript/Date/index.js
@@ -0,0 +1,40 @@
+// var date = new Date()
+
+// var days=
+// ["sunday", "mon", "tuesday", "wed", "thurs", "fri", "sat"]
+
+// var day_index = date.getDay()
+
+
+// console.log(days[day_index])
+
+
+
+
+// var date = new Date()
+// var newYear = new Date("January 1, 2020")
+
+// console.log(date.getDay())
+// console.log(date.getFullYear())
+// console.log(date.getHours())
+// console.log(date.getMinutes())
+// console.log(date.getTime())
+// console.log("------------------------------")
+// console.log(newYear.getDay())
+// console.log(newYear.getFullYear())
+// console.log(newYear.getHours())
+// console.log(newYear.getMinutes())
+// console.log(newYear.getTime())
+var date = new Date()
+var date = new Date("June 30, 2026")
+var saleEnd = new Date(2020,1)
+console.log(saleEnd)
+
+
+saleEnd.setHours(10)
+saleEnd.setFullYear(2025)
+saleEnd.setDate(12)
+saleEnd.setMinutes(102)
+saleEnd.setSeconds(43)
+
+console.log(saleEnd)
