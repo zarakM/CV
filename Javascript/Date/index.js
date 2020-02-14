1332cd6c251374088491057dae74c53c64abc52e
zarakM <mughalzarak@gmail.com>
2020-02-14 20:39:08 +0500

    Date class done

--- Javascript/Date/index.js
+++ Javascript/Date/index.js
@@ -1,49 +1,64 @@
-// var date = new Date()
+// // var date = new Date()
 
-// var days=
-// ["sunday", "mon", "tuesday", "wed", "thurs", "fri", "sat"]
+// // var days=
+// // ["sunday", "mon", "tuesday", "wed", "thurs", "fri", "sat"]
 
-// var day_index = date.getDay()
-
-
-// console.log(days[day_index])
+// // var day_index = date.getDay()
 
 
+// // console.log(days[day_index])
 
 
-// var date = new Date()
-// var newYear = new Date("January 1, 2020")
-
-// console.log(date.getDay())
-// console.log(date.getFullYear())
-// console.log(date.getHours())
-// console.log(date.getMinutes())
-// console.log(date.getTime())
-// console.log("------------------------------")
-// console.log(newYear.getDay())
-// console.log(newYear.getFullYear())
-// console.log(newYear.getHours())
-// console.log(newYear.getMinutes())
-// console.log(newYear.getTime())
-// var date = new Date()
-// var date = new Date("June 30, 2026")
-// var saleEnd = new Date(2020,1)
-// console.log(saleEnd)
 
 
-// saleEnd.setHours(10)
-// saleEnd.setFullYear(2025)
-// saleEnd.setDate(12)
-// saleEnd.setMinutes(102)
-// saleEnd.setSeconds(43)
+// // var date = new Date()
+// // var newYear = new Date("January 1, 2020")
 
-// console.log(saleEnd)
+// // console.log(date.getDay())
+// // console.log(date.getFullYear())
+// // console.log(date.getHours())
+// // console.log(date.getMinutes())
+// // console.log(date.getTime())
+// // console.log("------------------------------")
+// // console.log(newYear.getDay())
+// // console.log(newYear.getFullYear())
+// // console.log(newYear.getHours())
+// // console.log(newYear.getMinutes())
+// // console.log(newYear.getTime())
+// // var date = new Date()
+// // var date = new Date("June 30, 2026")
+// // var saleEnd = new Date(2020,1)
+// // console.log(saleEnd)
 
-function add(one, two) {
-    console.log(one + two)
-}
 
-add(8, 10)
-add(2, 19)
-add(5, 93)
-add(10, 49)
+// // saleEnd.setHours(10)
+// // saleEnd.setFullYear(2025)
+// // saleEnd.setDate(12)
+// // saleEnd.setMinutes(102)
+// // saleEnd.setSeconds(43)
+
+// // console.log(saleEnd)
+
+// function add(one, two) {
+//     console.log(one + two)
+// }
+
+// add(8, 10)
+// add(2, 19)
+// add(5, 93)
+// add(10, 49)
+
+
+
+
+// // 14,febuarary,2020 20:04
+
+
+var object = new Date()
+var date = object.getDate()
+var array_month = ["jan","feb","mar"]
+var month = object.getMonth()
+var year = object.getFullYear()
+
+
+console.log(date+","+ array_month[month]+","+year)
