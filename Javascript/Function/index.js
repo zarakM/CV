1b5bd9e73c8c04bf8c88e7ea8db57eac5e58d32b
zarakM <mughalzarak@gmail.com>
2020-02-14 21:38:28 +0500

    class: function done

---
+++ Javascript/Function/index.js
@@ -0,0 +1,62 @@
+
+// // function length_of_string(characters) {
+// //     return characters.length
+// // }
+// // function add(operand_one, operand_two) {
+// //     return operand_one + operand_two
+// // }
+// // function alerts() {
+// //     return 8 + 8
+// // }
+
+// // var string = "Hello"
+// // var length = length_of_string(string)
+// // var number = alerts()
+
+// // var one = 67
+// // var two = 33
+// // var addition = add(one, two)
+
+// // console.log(add(100))
+
+// console.log(nji)
+
+// var nji =10
+
+
+// var total = calcTot(110)
+
+// console.log( total )
+// console.log( calcTot(30) )
+// console.log(calcTot(60))
+
+
+
+// function tax(merchTot) {
+//     return 0.03 * merchTot
+// }
+
+// function calcTot(merchTot) {
+//     var orderTot;
+//     if (merchTot >= 100) {
+//         orderTot = merchTot;
+//     }
+//     else if (merchTot < 50.01) {
+//         orderTot = merchTot + 5;
+//     }
+//     else {
+//         console.log(tax(merchTot))
+//         orderTot = merchTot + 5 + tax(merchTot);
+//     }
+//     return orderTot
+// }
+
+
+function show() {
+    var name = "zarak"
+    console.log(name)
+}
+
+console.log(name)
+
+show()
