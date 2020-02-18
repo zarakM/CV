66e0e52f3c1eea3a102d990ed893912158f46d11
zarakM <mughalzarak@gmail.com>
2020-02-18 21:38:04 +0500

    class : Assignment and switch completed

--- Javascript/Function/index.js
+++ Javascript/Function/index.js
@@ -1,62 +1,103 @@
 
-// // function length_of_string(characters) {
-// //     return characters.length
+// // // function length_of_string(characters) {
+// // //     return characters.length
+// // // }
+// // // function add(operand_one, operand_two) {
+// // //     return operand_one + operand_two
+// // // }
+// // // function alerts() {
+// // //     return 8 + 8
+// // // }
+
+// // // var string = "Hello"
+// // // var length = length_of_string(string)
+// // // var number = alerts()
+
+// // // var one = 67
+// // // var two = 33
+// // // var addition = add(one, two)
+
+// // // console.log(add(100))
+
+// // console.log(nji)
+
+// // var nji =10
+
+
+// // var total = calcTot(110)
+
+// // console.log( total )
+// // console.log( calcTot(30) )
+// // console.log(calcTot(60))
+
+
+
+// // function tax(merchTot) {
+// //     return 0.03 * merchTot
 // // }
-// // function add(operand_one, operand_two) {
-// //     return operand_one + operand_two
+
+// // function calcTot(merchTot) {
+// //     var orderTot;
+// //     if (merchTot >= 100) {
+// //         orderTot = merchTot;
+// //     }
+// //     else if (merchTot < 50.01) {
+// //         orderTot = merchTot + 5;
+// //     }
+// //     else {
+// //         console.log(tax(merchTot))
+// //         orderTot = merchTot + 5 + tax(merchTot);
+// //     }
+// //     return orderTot
 // // }
-// // function alerts() {
-// //     return 8 + 8
-// // }
-
-// // var string = "Hello"
-// // var length = length_of_string(string)
-// // var number = alerts()
-
-// // var one = 67
-// // var two = 33
-// // var addition = add(one, two)
-
-// // console.log(add(100))
-
-// console.log(nji)
-
-// var nji =10
 
 
-// var total = calcTot(110)
-
-// console.log( total )
-// console.log( calcTot(30) )
-// console.log(calcTot(60))
-
-
-
-// function tax(merchTot) {
-//     return 0.03 * merchTot
+// function show() {
+//     var name = "zarak"
+//     console.log(name)
 // }
 
-// function calcTot(merchTot) {
-//     var orderTot;
-//     if (merchTot >= 100) {
-//         orderTot = merchTot;
-//     }
-//     else if (merchTot < 50.01) {
-//         orderTot = merchTot + 5;
-//     }
-//     else {
-//         console.log(tax(merchTot))
-//         orderTot = merchTot + 5 + tax(merchTot);
-//     }
-//     return orderTot
-// }
+// console.log(name)
 
+// show()
 
-function show() {
-    var name = "zarak"
-    console.log(name)
+var cities = ["karachi"]
+var country = ["pakistan"]
+
+function create(array, data) {
+    array.unshift(data)
 }
 
-console.log(name)
+function read(array) {
+    return array
+}
 
-show()
+function update(array ,index ,data) {
+    array[index]=data
+}
+
+function deletes(array, index) {
+    array.splice(index,1)
+}
+
+create(country,"lahore")
+create(country,"islamabad")
+create(country,"islamabad")
+create(country,"islamabad")
+create(country,"islamabad")
+create(country,"islamabad")
+create(country,"quetta")
+create(country,"Sri lanka")
+
+update(cities,1,"murree")
+
+deletes(cities,0)
+
+console.log(read(cities))
+console.log(read(country))
+
+var index = country.indexOf("lahore")
+
+country.splice(index,1)
+
+console.log(read(country))
---
+++ Javascript/Switch/index.js
@@ -0,0 +1,17 @@
+
+var day = "tuesday"
+
+
+switch (day) {
+    case "sunday":
+        console.log("Its weekend")
+        break
+    case "monday":
+        console.log("Its bad day")
+        break
+    case "friday":
+        console.log("good days coming")
+        break
+    default:
+        console.log("wrong day")
+}
